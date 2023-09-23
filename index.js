88849bd66713b4654333d66b2017e802ea451cbe
Tushard0304 <dubeytushar531@gmail.com>
2023-09-23 12:50:29 +0530

    last Commit

--- index.js
+++ index.js
@@ -1,65 +1,96 @@
-let isDOBopen = false;
-let DOB ;
-const setiEl = document.getElementById("set-i");
-const setcEl = document.getElementById("set-c");
-const firstcontEl = document.getElementById("firstcont");
-const secondcontEl = document.getElementById("secondcont");
-const dobinputEl = document.getElementById("dobinput");
-const dobbtnEl = document.getElementById("dobbtn");
-const yearEl = document.getElementById("years");
-const monthEl = document.getElementById("months");
-const dayEl = document.getElementById("days");
-const hourEl = document.getElementById("hours");
-const minuteEl = document.getElementById("minutes");
-const secondEl = document.getElementById("seconds");
+let isDOBOpen = false;
+let dateOfBirth;
+const settingCogEl = document.getElementById("settingIcon");
+const settingContentEl = document.getElementById("settingContent");
+const initialTextEl = document.getElementById("initialText");
+const afterDOBBtnTxtEl = document.getElementById("afterDOBBtnTxt");
+const dobButtonEl = document.getElementById("dobButton");
+const dobInputEl = document.getElementById("dobInput");
 
+const yearEl = document.getElementById("year");
+const monthEl = document.getElementById("month");
+const dayEl = document.getElementById("day");
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
 
+console.log(localStorage.getItem("year"));
 
-const toogleDOBselector =  () => {
-    
-    if(isDOBopen){
-        setcEl.classList.add("hide");
-    }
-    else{
-        setcEl.classList.remove("hide");
-    }
-    isDOBopen = !isDOBopen
-
+const makeTwoDigitNumber = (number) => {
+  return number > 9 ? number : `0${number}`;
 };
 
+const toggleDateOfBirthSelector = () => {
+  if (isDOBOpen) {
+    settingContentEl.classList.add("hide");
+  } else {
+    settingContentEl.classList.remove("hide");
+  }
+  isDOBOpen = !isDOBOpen;
 
-
-const setDOBhandler = () => {
-    DOB = dobinputEl.value;
-    if(DOB){
-        firstcontEl.classList.add("hide") 
-        secondcontEl.classList.remove("hide")
-
-    }
-    else{
-        firstcontEl.classList.remove("hide") 
-        secondcontEl.classList.add("hide")
-    }
-}
+  console.log("Toggle", isDOBOpen);
+};
 
 const updateAge = () => {
-    const currentage = new Date();
-    const datediff = currentage - DOB
-    const year = Math.floor(datediff / (1000*60*60*24*365))
-    const month = Math.floor(datediff / (1000*60*60*24*365))%12
-    const day = Math.floor(datediff / (1000*60*60*24))%30
-    const hour = Math.floor(datediff / (1000*60*60))%24
-    const minute = Math.floor(datediff / (1000*60))%60
-    const second = Math.floor(datediff / 1000) % 60
+  const currentDate = new Date();
+  const dateDiff = currentDate - dateOfBirth;
+  const year = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365));
+  const month = Math.floor((dateDiff / (1000 * 60 * 60 * 24 * 365)) % 12);
+  const day = Math.floor(dateDiff / (1000 * 60 * 60 * 24)) % 30;
+  const hour = Math.floor(dateDiff / (1000 * 60 * 60)) % 24;
+  const minute = Math.floor(dateDiff / (1000 * 60)) % 60;
+  const second = Math.floor(dateDiff / 1000) % 60;
 
-    
-}
-updateAge();
+  yearEl.innerHTML = makeTwoDigitNumber(year);
+  monthEl.innerHTML = makeTwoDigitNumber(month);
+  dayEl.innerHTML = makeTwoDigitNumber(day);
+  hourEl.innerHTML = makeTwoDigitNumber(hour);
+  minuteEl.innerHTML = makeTwoDigitNumber(minute);
+  secondEl.innerHTML = makeTwoDigitNumber(second);
+};
 
+const localStorageGetter = () => {
+  const year = localStorage.getItem("year");
+  const month = localStorage.getItem("month");
+  const date = localStorage.getItem("date");
+  if (year && month && date) {
+    dateOfBirth = new Date(year, month, date);
+  }
 
-setiEl.addEventListener('click',toogleDOBselector);
-dobbtnEl.addEventListener('click',setDOBhandler);
+  updateAge();
+};
 
+const contentToggler = () => {
+  updateAge();
+  if (dateOfBirth) {
+    initialTextEl.classList.add("hide");
+    afterDOBBtnTxtEl.classList.remove("hide");
+  } else {
+    afterDOBBtnTxtEl.classList.add("hide");
+    initialTextEl.classList.remove("hide");
+  }
+};
+
+const setDOBHandler = () => {
+  const dateString = dobInputEl.value;
+
+  dateOfBirth = dateString ? new Date(dateString) : null;
+
+  if (dateOfBirth) {
+    localStorage.setItem("year", dateOfBirth.getFullYear());
+    localStorage.setItem("month", dateOfBirth.getMonth());
+    localStorage.setItem("date", dateOfBirth.getDate());
+  }
+
+  contentToggler();
+  setInterval(() => updateAge(), 1000);
+};
+
+localStorageGetter();
+contentToggler();
+
+settingCogEl.addEventListener("click", toggleDateOfBirthSelector);
+dobButtonEl.addEventListener("click", setDOBHandler);
 
 
 
